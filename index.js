d16134a405a07bb32f9af000cb861dbb809f29f9
Ido Ben-Harosh <ido.b@mce-sys.com>
2020-04-24 12:56:54 +0300

    cosmetic changes for macOS

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ let currentWin;
 let tray = null
 app.on('window-all-closed', e => e.preventDefault())
 app.whenReady().then(() => {
-	tray = new Tray(path.resolve(__dirname, "js.png"));
+	tray = new Tray(path.resolve(__dirname, "js16.png"));
 	const contextMenu = Menu.buildFromTemplate([
 		{ label: 'Exit', type: 'normal', click: () => { app.quit() } },
 	])
 	tray.setToolTip('js-this! is running - Copy and Cmd/Ctrl+Shift+J')
 	tray.setContextMenu(contextMenu);
 	tray.displayBalloon({
+		icon: path.resolve(__dirname, "js.png"),
+		iconType: "custom",
 		title: "js-this! started",
 		content: `Copy and CmdOrCtrl+Shift+J to manipulate`,
 	});
